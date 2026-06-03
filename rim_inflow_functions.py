9bdae6fe739e2ca82b19c0da04e01817d458bd26
John Saunders <jsaunders@usbr.gov>
2026-06-03 14:35:34 -0600

    Added SFM005

--- rim_inflow_functions.py
+++ rim_inflow_functions.py
@@ -3,6 +3,37 @@ import numpy as np
 from extension_functions import create_final_flow_plots, area_scale, remove_negatives_timeseries
 from datetime import datetime
 
+def I_SFM005(df_extended_data, df_rim_inflows):
+    """
+    Calculate the final rim inflow for CalSim. Location: I_SFM005
+
+    Parameters
+    ----------
+    df_extended_data: dataframe
+        Dataframe of the extended data to pull from
+    df_rim_inflows: dataframe
+        Dataframe of rim inflows that have been calculated already
+
+    Returns
+    -------
+    None
+    """
+    # pull out the relevant station
+    df_location = df_extended_data['11318500']
+
+    # set anything negative to zero
+    df_location.loc[df_location < 0] = 0
+
+    # round to two decimal places
+    df_location = df_location.round(2)
+
+    # add into the rim inflow dataframe
+    df_rim_inflows['I_SFM005'] = df_location
+
+    # create the plots to compare the observed vs synthetic data
+    create_final_flow_plots(df_location, list(range(1934, 2025)), 'I_SFM005')
+
+
 def I_MFM008(df_full_data, df_rim_inflows):
     """
     Calculate the final rim inflow for CalSim. Location: I_MFM008
@@ -11,6 +42,8 @@ def I_MFM008(df_full_data, df_rim_inflows):
     ----------
     df_full_data: dataframe
         Dataframe of the gauge data to pull from
+    df_rim_inflows: dataframe
+        Dataframe of rim inflows that have been calculated already
 
     Returns
     -------
