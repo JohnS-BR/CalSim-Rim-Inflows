f20f7b89eb37ed8fd83e81c8b3eebb34c2036c70
John Saunders <jsaunders@usbr.gov>
2026-08-06 13:04:01 -0600

    Made a clean version of the upper mokelumne (consumnes/mokelumne) without many of the quirks of the sheets, keeping the same logic. Reduced multiple versions of some data sets down to a single version. Eliminated some clear errors.

--- upper_mokelumne_calculate_rim_inflows.py
+++ upper_mokelumne_calculate_rim_inflows.py
@@ -14,23 +14,6 @@ if __name__ == "__main__":
     # option to plot comparison
     b_compare_data = True
 
-    # option to run each element using "upstream" (antecedent) SV INPUT values from sheets rather than from the values
-    # calculated within the Python code.
-    b_use_upstream_sv_inputs = True
-
-    # This flag initiates two things: 1) a check to see that the upstream code up until we reach the inputs for the s-curve
-    # disaggregation match the excel workbooks. It checks the x watershed and y watershed against data in files that end
-    # in "input_to_s_curve.csv".  2) It takes the s-curve from the sheets (found in files that end in
-    # "output_from_s_curve.csv") and runs the code downstream of that point. It compares the final output to the
-    # SV INPUT tab of the sheets (found in "CS3_SJR_ReadAllInflowDatatoDSS_05.17.23.xlsm").
-    b_replicate_sheets = True
-
-    # true on this b_reproduce_error_lbear_ss reproduces two errors in the sheets. 1) time shifts the monthly averages
-    # relative to where they belong by 3 months to replicate sheet. 2) calculates monthly averages with an incorrect
-    # denominator. The flag is set at the top of this document. Set this to false to run a more correct version of
-    # I_SLTSP.
-    b_reproduce_error_lbear_ss = True
-
     # --- End Flags
 
     # this holds the already extended evap rates
@@ -41,8 +24,6 @@ if __name__ == "__main__":
     s_prev_rim_inflows_fn = "CS3_SJR_ReadAllInflowDatatoDSS_05.17.23.csv"
     s_prev_rim_inflow_sheet = "Inflows"
 
-    # path for some extra sv inputs
-    s_prev_rim_inflows_extra = r".\Inputs\upper_mokelumne_2022_sv_inputs.csv"
     # first if the needed output folders don't exist, create them
     os.makedirs('./Intermediate', exist_ok=True)
     os.makedirs('./Figures', exist_ok=True)
@@ -51,41 +32,6 @@ if __name__ == "__main__":
 
     # read in the data that we already read in
     df_full_data = pd.read_csv('./Intermediate/upper_mokelumne_full_gauge_data.csv', index_col=0, parse_dates=True)
-
-    # read in upstream sheet SV INPUT sheet data
-    if b_use_upstream_sv_inputs:
-        # read in data
-        df_sv_inputs = pd.read_csv(s_prev_rim_inflows_fn, index_col=0, parse_dates=True)
-        df_extra_sv_inputs = pd.read_csv(s_prev_rim_inflows_extra, index_col=0, parse_dates=True)
-
-    if b_replicate_sheets:
-        # read in files that contain the sheet data from just before and just after the s-curve process, to isolate that
-        # factor. The "after s" files are the data from the sheets after merging the synthetic output into the gaps in
-        # the data.
-
-        # create a list of lists. inner elements are ['column_name', 'path to before csv', 'path to after csv']
-        ls_sheet_info = [['COL003', './Inputs/s_curve_replication/col003_input_to_s_curve.csv',
-                          './Inputs/s_curve_replication/col003_output_from_s_curve.csv'],
-                         ['SLTSP', './Inputs/s_curve_replication/sltsp_input_to_s_curve.csv',
-                          './Inputs/s_curve_replication/sltsp_output_from_s_curve.csv'],
-                         ['UBEAR', './Inputs/s_curve_replication/ubear_input_to_s_curve.csv',
-                         './Inputs/s_curve_replication/ubear_output_from_s_curve.csv'],
-                         ['NFM010', './Inputs/s_curve_replication/nfm010_input_to_s_curve.csv',
-                         './Inputs/s_curve_replication/nfm010_output_from_s_curve.csv'],
-                         ['TGC003', './Inputs/s_curve_replication/nfm010_input_to_s_curve.csv',
-                         './Inputs/s_curve_replication/tgc003_output_from_s_curve.csv'],
-                         ['CMP001', './Inputs/s_curve_replication/cmp001_input_to_s_curve.csv',
-                          './Inputs/s_curve_replication/cmp001_output_from_s_curve.csv'],
-                         ['CMP014', './Inputs/s_curve_replication/cmp014_input_to_s_curve.csv',
-                         './Inputs/s_curve_replication/cmp014_output_from_s_curve.csv'],
-                         ['DEE023', './Inputs/s_curve_replication/dee023_input_to_s_curve.csv',
-                         './Inputs/s_curve_replication/dee023_output_from_s_curve.csv']
-                         ]
-        # create the dataframes where we keep the before and after data
-        df_before_s = pd.DataFrame()
-        df_after_s = pd.DataFrame()
-
-        read_replication_data(ls_sheet_info, df_before_s, df_after_s)
 
     # ----------------------------------
     # --- GAP FILL AND DROP, ROUND 1 ---
@@ -104,21 +50,23 @@ if __name__ == "__main__":
     # --- MERGE AND COPY GAUGES ---
     # -----------------------------
 
-    # -- begin COL003 merge --
+    # -- begin 11319500 merges --
+    #### these two versions of 11319500 use different data sources (USGS vs EBMUD) and different gap filling. Neither one
+    #### is obviously better at first glance.
     # with COL003 (11319500), EBMUD is main historical gage (pre-2021) but NaN's are filled with CDEC MKM
     # Later the filled out USGS 11319500 is used for s-curve on the COL003
     # gage, which is USGS 11315000. Version 1 is used for COL003
     if '11319500_v1' not in df_full_data.columns:
         df_full_data['11319500_v1'] = np.nan
     df_full_data['11319500_v1'] = df_full_data['EBMUD_11319500'].fillna(df_full_data['MKM'])
-    # -- end COL003 merge --
 
     # 11319500_v2 is created for use in MOK079. This is the USGS gage 11319500. Compare with COL003 above.
     if '11319500_v2' not in df_full_data.columns:
         df_full_data['11319500_v2'] = np.nan
     df_full_data['11319500_v2'] = df_full_data['11319500'].copy()
+    # -- end 11319500 merges --
 
-    # for JNKSN, create a copy of data with dropped WY1955
+    # for JNKSN, create a copy of data with dropped WY1955. Sheet said "Do not use '55 as flow impacted by dam."
     df_full_data.rename(columns={'11332500': '11332500_v1'}, inplace=True)
     df_full_data["11332500_v2"] = df_full_data["11332500_v1"].copy()
     df_full_data.loc["1954-10-31":"1955-10-01", "11332500_v2"] = float("nan")
@@ -154,16 +102,12 @@ if __name__ == "__main__":
     print("Calculating unimpaired flows, round 1 ...")
 
     # see the top of this doc for details on the lbear_ss errors.
-    df_unimpaired_data['LBearSS_v1'] = unimpaired_lbear_salt_springs_fnf_v1(df_full_data,
-                                            b_reproduce_error_lbear_ss=b_reproduce_error_lbear_ss)      # see SLTSP
-    df_unimpaired_data['LBearSS_v2'] = unimpaired_lbear_salt_springs_fnf_v2(df_full_data,
-                                            b_reproduce_error_lbear_ss=b_reproduce_error_lbear_ss)      # see UBEAR
+    df_unimpaired_data['LBearSS'] = unimpaired_lbear_salt_springs_fnf(df_full_data)              # see SLTSP and UBEAR
     df_unimpaired_data['11309500'] = unimpaired_11309500_for_NHGAN(df_full_data)                        # see NHGAN
     df_unimpaired_data['NF_SF_ITAS'] = unimpaired_NF_SF_ITAS(df_full_data)                              # see NHGAN
     df_unimpaired_data['NH_DAM_RELEASE'] = unimpaired_NH_DAM_RELEASE(df_full_data)                      # see NHGAN
     df_unimpaired_data['11319500_v2'] = unimpaired_11319500_v2(df_full_data)                            # see MOK079
-    df_unimpaired_data['11335000_v1'] = unimpaired_11335000(df_full_data, b_v1=True)                    # see CMP001
-    df_unimpaired_data['11335000_v2'] = unimpaired_11335000(df_full_data, b_v1=False)                   # see JNKSN
+    df_unimpaired_data['11335000'] = unimpaired_11335000(df_full_data)                                  # see CMP001
     df_unimpaired_data['11333000'] = unimpaired_11333000(df_full_data)                                  # see CMP001
 
 
@@ -178,9 +122,9 @@ if __name__ == "__main__":
     # ----------------------------------
 
 
-    # see DEE023, 11335700 WY1967 is gap filled with WY1965 times a ratio of annual flows from 11335000_v2
-    d_65 = df_unimpaired_data.loc['1964-10-01':'1965-10-01', '11335000_v2'].sum()     # annual flow for WY65
-    d_67 = df_unimpaired_data.loc['1966-10-01':'1967-10-01', '11335000_v2'].sum()     # annual flow for WY67
+    # see DEE023, 11335700 WY1967 is gap filled with WY1965 times a ratio of annual flows from 11335000
+    d_65 = df_unimpaired_data.loc['1964-10-01':'1965-10-01', '11335000'].sum()     # annual flow for WY65
+    d_67 = df_unimpaired_data.loc['1966-10-01':'1967-10-01', '11335000'].sum()     # annual flow for WY67
     df_shifted = df_full_data['11335700'].copy()
     # create a copy of the data shifted forward 2 years
     df_shifted.index = df_shifted.index + pd.DateOffset(years=2)
@@ -197,40 +141,28 @@ if __name__ == "__main__":
     df_extended_data = pd.DataFrame()
     df_synthetic_data = pd.DataFrame()
 
-
-    if b_replicate_sheets:
-        print("Checking inputs to s-curve, part 1...")
-        compare_two_df(df_full_data['11317000'].drop(df_full_data['11317000'].index[0]).round(2), df_sv_inputs['I_MFM008'], '11317000',
-                       'SV_INPUT_MFM008')                                                   # for MFM008
-        compare_two_df(df_unimpaired_data['11335000_v1'], df_before_s['CMP001'], '11335000_v1',
-                       'before_s_CMP001')                                                   # for CMP001
-        compare_two_df(df_unimpaired_data['11335000_v2'], df_before_s['CMP014'], '11335000_v2',
-                       'before_s_CMP014')                                                   # for CMP014
-
-
-
     print("Extending flows, part 1...")
     # extend with the s-curve disaggregation, round 1
     extend_data(df_full_data['11317000'], df_full_data['11318500'],
                 df_extended_data, df_synthetic_data, 1934, i_final_year, False,
                 '11318500', i_final_year=i_final_year)                                              # see SFM005
     # for JNKSN, s-curve
-    extend_data(df_unimpaired_data['11335000_v2'], df_full_data['11332500_v2'],
+    extend_data(df_unimpaired_data['11335000'], df_full_data['11332500_v2'],
                 df_extended_data, df_synthetic_data, 1947, 1954, False,
                 '11332500', i_final_year=i_final_year)                                               # see JNKSN
-    extend_data(df_unimpaired_data['11335000_v1'], df_unimpaired_data['11333000'],
+    extend_data(df_unimpaired_data['11335000'], df_unimpaired_data['11333000'],
                 df_extended_data, df_synthetic_data, 1956, 2004, False,
                 '11333000', i_final_year=i_final_year)                                               # see CMP001
-    extend_data(df_unimpaired_data['11335000_v2'], df_full_data['11331500'],
+    extend_data(df_unimpaired_data['11335000'], df_full_data['11331500'],
                 df_extended_data, df_synthetic_data, 1949, 1954, False,
                 '11331500', i_final_year=i_final_year)                                               # see CMP014
-    extend_data(df_unimpaired_data['11335000_v2'], df_full_data['11326300'],
+    extend_data(df_unimpaired_data['11335000'], df_full_data['11326300'],
                 df_extended_data, df_synthetic_data, 1961, 1970, False,
                 '11326300', i_final_year=i_final_year)                                               # see DSC035
-    extend_data(df_unimpaired_data['11335000_v2'], df_full_data['11327000_v2'],
+    extend_data(df_unimpaired_data['11335000'], df_full_data['11327000_v2'],
                 df_extended_data, df_synthetic_data, 1961, 1980, False,
                 '11327000_v2', i_final_year=i_final_year)                                            # see DSC035
-    extend_data(df_unimpaired_data['11335000_v2'], df_full_data['11335700'],
+    extend_data(df_unimpaired_data['11335000'], df_full_data['11335700'],
                 df_extended_data, df_synthetic_data, 1961, 1977, False,
                 '11335700', i_final_year=i_final_year, s_strange_sheet='DEE023')                     # see DEE023
 
@@ -254,13 +186,6 @@ if __name__ == "__main__":
 
     print("Extending flows, part 2...")
 
-    if b_replicate_sheets:
-        print("Checking inputs to s-curve, part 2...")
-        compare_two_df(df_before_s['COL003'], df_unimpaired_data['11319500_v1'], 'col003_before_s',
-                       '11319500_v1')                                                           # see COL003
-        compare_two_df(df_before_s['SLTSP'], df_unimpaired_data['11319500_v1'], 'sltsp_before_s',
-                       '11319500_v1')                                                           # see SLTSP
-
     extend_data_multi_model(df_unimpaired_data['11319500_v1'], df_full_data['11318500'], df_full_data['11317000'],
                             df_full_data['11318500'], df_extended_data, df_synthetic_data,
                             1934, 2021, 1934, 2021, False, '11318500',
@@ -269,12 +194,9 @@ if __name__ == "__main__":
     extend_data(df_unimpaired_data['11319500_v1'], df_full_data['11315000'],
                df_extended_data, df_synthetic_data, 1928, i_final_year, False,
                '11315000', i_x_start_year=1922, i_final_year=i_final_year, s_strange_sheet='COL003') # see COL003
-    extend_data(df_unimpaired_data['11319500_v1'], df_unimpaired_data['LBearSS_v1'],
+    extend_data(df_unimpaired_data['11319500_v1'], df_unimpaired_data['LBearSS'],
                df_extended_data, df_synthetic_data, 1989, i_final_year, False,
-               'LBearSS_v1', i_final_year=i_final_year)                                              # see SLTSP
-    extend_data(df_unimpaired_data['11319500_v1'], df_unimpaired_data['LBearSS_v2'],
-                df_extended_data, df_synthetic_data, 1989, i_final_year, False,
-                'LBearSS_v2', i_final_year=i_final_year)                                             # see UBEAR
+               'LBearSS', i_final_year=i_final_year)                                              # see SLTSP and UBEAR
     extend_data(df_unimpaired_data['11319500_v1'], df_unimpaired_data['11316600'],
                 df_extended_data, df_synthetic_data, 1986, i_y_end_year=2001,
                 b_use_all_y_data=False, s_name='11316600', i_final_year=i_final_year)                       # see NFM010
@@ -304,51 +226,25 @@ if __name__ == "__main__":
 
     print("Calculating rim inflows...")
 
-    if b_replicate_sheets:
-        I_MFM008(df_full_data, df_rim_inflows)
-        I_SFM005(df_extended_data, df_rim_inflows)
-        I_COL003(df_after_s[['COL003']].rename(columns={"COL003":"11315000"}), df_rim_inflows)
-        I_SLTSP(df_after_s['SLTSP'], df_sv_inputs['I_COL003'], df_rim_inflows)
-        I_UBEAR(df_after_s['UBEAR'], df_sv_inputs['I_COL003'], df_rim_inflows)
-        I_NFM010(df_after_s['NFM010'], df_rim_inflows)
-        I_TGC003(df_sv_inputs['I_NFM010'], df_rim_inflows)
-        I_NHGAN(df_unimpaired_data[['11309500']], df_unimpaired_data[['NF_SF_ITAS']],
-                df_unimpaired_data[['NH_DAM_RELEASE']], df_rim_inflows)
-        I_PARDE(df_rim_inflows)
-        I_CMCHE(df_rim_inflows)
-        I_MOK079(df_unimpaired_data[['11319500_v2']], df_full_data[['11323500']], df_sv_inputs[['I_PARDE']],
-                 df_extra_sv_inputs[['I_CMCHE_IN_MOK079']],
-                 df_sv_inputs[['I_NFM010']], df_sv_inputs[['I_MFM008']],
-                 df_sv_inputs[['I_UBEAR']], df_sv_inputs[['I_SLTSP']], df_sv_inputs[['I_SFM005']],
-                 df_sv_inputs[['I_TGC003']], df_sv_inputs[['I_COL003']], df_rim_inflows)
-        I_JNKSN(df_extended_data['11332500'], df_rim_inflows)
-        I_CMP001(df_after_s[['CMP001']], df_extra_sv_inputs[['I_JNKSN_IN_CMP001']], df_rim_inflows)
-        I_CMP014(df_after_s[['CMP001']], df_extra_sv_inputs[['I_JNKSN_IN_CMP001']], df_rim_inflows)
-        I_CSM035(df_unimpaired_data[['11335000_v1']], df_sv_inputs[['I_JNKSN']],df_sv_inputs[['I_CMP001']],
-                 df_sv_inputs[['I_CMP014']], df_rim_inflows)
-        I_AMADR(df_unimpaired_data[['11335000_v2']], df_rim_inflows)
-        I_DSC035(df_extended_data[['11326300']], df_extended_data[['11327000_v2']], df_rim_inflows)
-        I_DEE023(df_after_s[['DEE023']], df_rim_inflows)
-    else:
-        I_MFM008(df_full_data, df_rim_inflows)
-        I_SFM005(df_extended_data, df_rim_inflows)
-        I_COL003(df_extended_data, df_rim_inflows)
-        I_SLTSP(df_extended_data['LBearSS_v1'], df_extended_data['11315000'], df_rim_inflows)
-        I_UBEAR(df_extended_data['LBearSS_v2'], df_extended_data['11315000'], df_rim_inflows)
-        I_NFM010(df_extended_data['11316600'], df_rim_inflows)
-        I_TGC003(df_rim_inflows['I_NFM010'], df_rim_inflows)
-        I_NHGAN(df_unimpaired_data[['11309500']], df_unimpaired_data[['NF_SF_ITAS']],
-                df_unimpaired_data[['NH_DAM_RELEASE']], df_rim_inflows)
-        I_PARDE(df_rim_inflows)
-        I_CMCHE(df_rim_inflows)
-        I_JNKSN(df_extended_data['11332500'], df_rim_inflows)
-        I_CMP001(df_extended_data[['11333000']], df_rim_inflows[['I_JNKSN']], df_rim_inflows)
-        I_CMP014(df_extended_data[['11333000']], df_rim_inflows[['I_JNKSN']], df_rim_inflows)
-        I_CSM035(df_unimpaired_data[['11335000_v1']], df_rim_inflows[['I_JNKSN']], df_rim_inflows[['I_CMP001']],
-             df_rim_inflows[['I_CMP014']], df_rim_inflows)
-        I_AMADR(df_unimpaired_data[['11335000_v2']], df_rim_inflows)
-        I_DSC035(df_extended_data[['11326300']], df_extended_data[['11327000_v2']], df_rim_inflows)
-        I_DEE023(df_extended_data[['11335700']], df_rim_inflows)
+    I_MFM008(df_full_data, df_rim_inflows)
+    I_SFM005(df_extended_data, df_rim_inflows)
+    I_COL003(df_extended_data, df_rim_inflows)
+    I_SLTSP(df_extended_data['LBearSS'], df_extended_data['11315000'], df_rim_inflows)
+    I_UBEAR(df_extended_data['LBearSS'], df_extended_data['11315000'], df_rim_inflows)
+    I_NFM010(df_extended_data['11316600'], df_rim_inflows)
+    I_TGC003(df_rim_inflows['I_NFM010'], df_rim_inflows)
+    I_NHGAN(df_unimpaired_data[['11309500']], df_unimpaired_data[['NF_SF_ITAS']],
+            df_unimpaired_data[['NH_DAM_RELEASE']], df_rim_inflows)
+    I_PARDE(df_rim_inflows)
+    I_CMCHE(df_rim_inflows)
+    I_JNKSN(df_extended_data['11332500'], df_rim_inflows)
+    I_CMP001(df_extended_data[['11333000']], df_rim_inflows[['I_JNKSN']], df_rim_inflows)
+    I_CMP014(df_extended_data[['11333000']], df_rim_inflows[['I_JNKSN']], df_rim_inflows)
+    I_CSM035(df_unimpaired_data[['11335000']], df_rim_inflows[['I_JNKSN']], df_rim_inflows[['I_CMP001']],
+         df_rim_inflows[['I_CMP014']], df_rim_inflows)
+    I_AMADR(df_unimpaired_data[['11335000']], df_rim_inflows)
+    I_DSC035(df_extended_data[['11326300']], df_extended_data[['11327000_v2']], df_rim_inflows)
+    I_DEE023(df_extended_data[['11335700']], df_rim_inflows)
 
     df_rim_inflows.to_csv('./Outputs/upper_mokelumne_rim_inflows.csv')
 
@@ -390,7 +286,8 @@ if __name__ == "__main__":
         df_diffs = df_diffs.join(df_rmse)
 
         print("note the CMP014 will not show agreement in the following table because the python code is replicating the rev G version,")
-        print("but the CS3_SJR_ReadAllInflowDatatoDSS_05.17.23.xlsm file has the rev F version.")
+        print("but the CS3_SJR_ReadAllInflowDatatoDSS_05.17.23.xlsm file has the rev F version. In calculating CSM035, the inflows")
+        print("from CMP014 are subtracted near the end of the calculation, causing knock-on effects.")
 
         # format output
 
